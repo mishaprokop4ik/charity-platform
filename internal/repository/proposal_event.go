0ea0f02143911873f561267dc9e3da93aa46b91f
mykhailo.prokopchyk <mishaprokop4ik@gmail.com>
2023-03-22 23:03:48 +0200

    fixed error

--- internal/repository/proposal_event.go
+++ internal/repository/proposal_event.go
@@ -118,7 +118,7 @@ func (p *ProposalEvent) GetEventsWithSearchAndSort(ctx context.Context,
 	}
 
 	if searchValues.Tags != nil && len(*searchValues.Tags) != 0 {
-		if len(searchValues.GetTagsValues()) == 0 {
+		if len(searchValues.GetTagsValues()) == 0 && len(searchValues.GetTagsTitle()) != 0 {
 			subQuery := db.Table("tag").Select("event_id").
 				Where("LOWER(title) IN (?) AND event_type = ?", searchValues.GetTagsTitle(), models.ProposalEventType)
 
